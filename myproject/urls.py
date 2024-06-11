15ae3366a4b4a816af8c93c8913c72cc0d0bad05
youngjun <whdudwns7100@naver.com>
2024-06-11 16:42:27 +0900

    add : 회원가입

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
+    path('register', RegisterView.as_view(), name='register'),
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
---
+++ myproject/views.py
@@ -0,0 +1,21 @@
+# myapp/views.py
+
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "user": RegisterSerializer(user, context=self.get_serializer_context()).data,
+                "message": "User Created Successfully.  Now perform Login to get your token",
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
